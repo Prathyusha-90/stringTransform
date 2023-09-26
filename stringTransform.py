71982415f7281855992998b64330870e5b77dc56
Prathyusha-90 <145919144+Prathyusha-90@users.noreply.github.com>
2023-09-26 03:51:10 -0700

    Add files via upload

---
+++ stringTransform.py
@@ -0,0 +1,67 @@
+#Capitalize
+data = " this is prathyusha "
+print( data.capitalize())
+
+#Title
+name = " prathyusha reddy"
+print(name.title())
+
+#Upper
+name = " daddy "
+print(name.upper())
+
+#lower
+name = " MUMMY "
+print(name.lower())
+
+#casefold
+name = " JANU "
+print(name.casefold())
+
+#swapcase
+name = " mahabubnagar "
+print(name.swapcase())
+
+#isnumeric
+t = " 0617 "
+print(t.isnumeric())
+
+#isalnum
+v = " pallavi123 "
+print(v.isalnum())
+
+#isdecimal
+c = "2345"
+print(c.isdecimal())
+
+#isdigit
+a = " madhuranagar555599999666 "
+print(a.isdigit())
+
+#isascii
+s = " ramoji"
+print(s.isascii())
+
+#isupper
+b = " ramreddy "
+print(b.isupper())
+
+#islower
+c = " deepu "
+print(c.islower())
+
+#isspace
+s = " kadumuriprathyusha "
+print(s.isspace())
+
+#isidentifier
+q = "prathyu"
+print(q.isidentifier())
+
+#isprintable
+x = " pavani "
+print(x.isprintable())
+
+#istitle
+f = " peacock "
+print(f.istitle())
